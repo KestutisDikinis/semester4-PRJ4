c4b2b0d8f11aa9491ca349b5c75e69376b2d3916
Mark Brož <b.marekbroz@student.fontys.nl>
2021-04-18 16:41:28 +0200

    WIP profile page, working on nav. bar

--- Implementation/prj4Workspace/prj4Navigation/Profile.swift
+++ Implementation/prj4Workspace/prj4Navigation/Profile.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 //container view that shows all the content
 struct profileTest: View {
+    
+    @State var selection:Int = 0
+    
     var body: some View {
         //Vertical container that holds all elements
         VStack{
@@ -31,121 +34,135 @@ struct profileTest: View {
             
             Text("Nav. bar placeholder")
             
+            
             //for swiping between pages functionality
             ZStack{
-                TabView{
-                    //Embedded form for styling the HStacks
-                    //Profile info section
-                    Form {
-                        //Added sections for spacing between other sections and naming groups
+                VStack{
+                    //PLACEHOLDER FOR selection parameter for the nav bar to select the correct page
+                    TabView(){
+                        //Embedded form for styling the HStacks
+                        //Profile info section
+                        Form {
+                            //Added sections for spacing between other sections and naming groups
 
-                        Section(header: Text("PROFILE INFORMATION")){
-                            HStack{
-                                Text("First Name")
-                                Spacer()
-                                Text("Marek")
-                                    .foregroundColor(.gray)
-                                    .font(.callout)
+                            Section(header: Text("PROFILE INFORMATION")){
+                                HStack{
+                                    Text("First Name")
+                                    Spacer()
+                                    Text("Marek")
+                                        .foregroundColor(.gray)
+                                        .font(.callout)
+                                }
+                                HStack{
+                                    Text("Last Name")
+                                    Spacer()
+                                    Text("Brož")
+                                        .foregroundColor(.gray)
+                                        .font(.callout)
+                                }
+                                HStack{
+                                    Text("Email")
+                                    Spacer()
+                                    Text("JuliaSartori@student.fontys.nl")
+                                        .foregroundColor(.gray)
+                                        .font(.callout)
+                                }
+                                HStack{
+                                    Text("City")
+                                    Spacer()
+                                    Text("Venlo")
+                                        .foregroundColor(.gray)
+                                        .font(.callout)
+                                }
+                                HStack{
+                                    Text("Birthday")
+                                    Spacer()
+                                    Text("1/1/2015")
+                                        .foregroundColor(.gray)
+                                        .font(.callout)
+                                }
                             }
-                            HStack{
-                                Text("Last Name")
-                                Spacer()
-                                Text("Brož")
-                                    .foregroundColor(.gray)
-                                    .font(.callout)
-                            }
-                            HStack{
-                                Text("Email")
-                                Spacer()
-                                Text("JuliaSartori@student.fontys.nl")
-                                    .foregroundColor(.gray)
-                                    .font(.callout)
-                            }
-                            HStack{
-                                Text("City")
-                                Spacer()
-                                Text("Venlo")
-                                    .foregroundColor(.gray)
-                                    .font(.callout)
-                            }
-                            HStack{
-                                Text("Birthday")
-                                Spacer()
-                                Text("1/1/2015")
-                                    .foregroundColor(.gray)
-                                    .font(.callout)
+                            Section{
+                                //Added action button for user to interact with
+                                Button(action:  {
+                                        print("Edit Profile Information")
+                                }) {Text("Edit Profile Information")
+                                }
                             }
                         }
-                        Section{
-                            //Added action button for user to interact with
-                            Button(action:  {
-                                    print("Edit Profile Information")
-                            }) {Text("Edit Profile Information")
+                        //History group
+                        Form {
+                            //Section name
+                            Section(header: Text("ROUTES HISTORY")){
+                                //Spacing between each route entry
+                                HStack{
+                                    Image("route")
+                                        .resizable()
+                                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                                        .frame(width: 60, height: 60)
+                                        .clipped()
+                                        .cornerRadius(10)
+                                    VStack(alignment: .leading){
+                                        Text("TITLE + description")
+                                            .font(.system(size: 18, weight: .medium, design: .default))
+                                            .offset(y:5)
+                                        Text("89km")
+                                            .offset(y:20)
+                                        Text("21.2.2021")
+                                            .offset(x:175)
+                                    }
+                                }
+                                HStack{
+                                    Image("route")
+                                        .resizable()
+                                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                                        .frame(width: 60, height: 60)
+                                        .clipped()
+                                        .cornerRadius(10)
+                                    VStack(alignment: .leading){
+                                        Text("TITLE + description")
+                                            .font(.system(size: 18, weight: .medium, design: .default))
+                                            .offset(y:5)
+                                        Text("89km")
+                                            .offset(y:20)
+                                        Text("21.2.2021")
+                                            .offset(x:175)
+                                    }
+                                }
+                                HStack{
+                                    Image("route")
+                                        .resizable()
+                                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                                        .frame(width: 60, height: 60)
+                                        .clipped()
+                                        .cornerRadius(10)
+                                    VStack(alignment: .leading){
+                                        Text("TITLE + description")
+                                            .font(.system(size: 18, weight: .medium, design: .default))
+                                            .offset(y:5)
+                                        Text("89km")
+                                            .offset(y:20)
+                                        Text("21.2.2021")
+                                            .offset(x:175)
+                                    }
+                                }
                             }
                         }
+                    
                     }
-                    //History group
-                    Form {
-                        //Section name
-                        Section(header: Text("ROUTES HISTORY")){
-                            //Spacing between each route entry
-                            HStack{
-                                Image("route")
-                                    .resizable()
-                                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                                    .frame(width: 60, height: 60)
-                                    .clipped()
-                                    .cornerRadius(10)
-                                VStack(alignment: .leading){
-                                    Text("TITLE + description")
-                                        .font(.system(size: 18, weight: .medium, design: .default))
-                                        .offset(y:5)
-                                    Text("89km")
-                                        .offset(y:20)
-                                    Text("21.2.2021")
-                                        .offset(x:175)
-                                }
-                            }
-                            HStack{
-                                Image("route")
-                                    .resizable()
-                                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                                    .frame(width: 60, height: 60)
-                                    .clipped()
-                                    .cornerRadius(10)
-                                VStack(alignment: .leading){
-                                    Text("TITLE + description")
-                                        .font(.system(size: 18, weight: .medium, design: .default))
-                                        .offset(y:5)
-                                    Text("89km")
-                                        .offset(y:20)
-                                    Text("21.2.2021")
-                                        .offset(x:175)
-                                }
-                            }
-                            HStack{
-                                Image("route")
-                                    .resizable()
-                                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                                    .frame(width: 60, height: 60)
-                                    .clipped()
-                                    .cornerRadius(10)
-                                VStack(alignment: .leading){
-                                    Text("TITLE + description")
-                                        .font(.system(size: 18, weight: .medium, design: .default))
-                                        .offset(y:5)
-                                    Text("89km")
-                                        .offset(y:20)
-                                    Text("21.2.2021")
-                                        .offset(x:175)
-                                }
-                            }
+                    .tabViewStyle(PageTabViewStyle())
+                    
+                    //NOT WORKING
+                    Button(action: {
+                        withAnimation{
+                            selection = 1
                         }
+                    }) {
+                        Text("go home")
                     }
-                
                 }
-                .tabViewStyle(PageTabViewStyle())
-            }
+                }
+               
             
             
         }
@@ -155,6 +172,10 @@ struct profileTest: View {
 //Renders the code avove in a preview window
 struct Profile_Previews: PreviewProvider {
     static var previews: some View {
-        profileTest()
+        Group {
+            profileTest()
+            profileTest()
+                .preferredColorScheme(.dark)
+        }
     }
 }
